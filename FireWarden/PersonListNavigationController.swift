814538dc97cb5cac278bb472bbf972492fdfc6a3
Paul Keller <paul.keller.it@gmail.com>
2017-08-17 16:46:50 +0800

    Working Person List View
    Data is retrieved and displayed correctly

--- FireWarden/PersonListNavigationController.swift
+++ FireWarden/PersonListNavigationController.swift
@@ -17,7 +17,7 @@ class PersonListNavigationController: UINavigationController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        prepareForNavigationSegue()
+        //prepareForNavigationSegue()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,130 +25,7 @@ class PersonListNavigationController: UINavigationController {
         // Dispose of any resources that can be recreated.
     }
     
-    func prepareForNavigationSegue() {
-        //let waitingView = showWaitingView(tableView: tableView)
-        
-        let url = URL(string: "http://www.gratuityp.com/pk/GetData.php")
-        var request = URLRequest(url: url!)
-        request.httpMethod = "POST"
-        let postString = "ScriptName=sp_PersonList_GetList&ParamString=''"
-        request.httpBody = postString.data(using: .utf8)
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if error != nil
-            {
-                print(error!)
-            }
-            else
-            {
-                defer {
-                    DispatchQueue.main.async {
-                        //waitingView.removeFromSuperview()
-                    }
-                }
-                if let content=data
-                {
-                    do
-                    {
-                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
-                        print(myJson)
-                        if myJson.count > 0
-                        {
-                            self.wardenList.removeAll()
-                            for item in myJson {
-                                let obj = item as! NSDictionary
-                                
-                                if obj.count <= 5 {
-                                    //It's a WardenCount obj
-                                    let wardenCountObj = WardenCount(wardenID: obj["WardenID"] as! Int32, firstName: obj["FirstName"] as! String, lastName: obj["LastName"] as! String, personCount: obj["PersonCount"] as! Int32, sectionNumber: Int32(obj["SectionNumber"] as! String)!)
-                                    print(wardenCountObj)
-                                    self.wardenList.append(wardenCountObj)
-                                    
-                                    let wardenPersonList = [Person]()
-                                    let listObj = PersonListObj()
-                                    listObj.wardenObj = wardenCountObj
-                                    listObj.personObj = wardenPersonList
-                                    
-                                    self.personList.append(listObj)
-                                    
-                                    //This creates an array of wardens with a blank personObj array
-                                    //the else part should then find the correct warden and append the personObj to that array
-                                }
-                                else
-                                {
-                                    //it's a PersonListObj
-                                    
-                                    let locationObj = Location(locationID: obj["PersonLocationID"] as! Int32, locationName: obj["LocationName"] as! String, floor: obj["Floor"] as! String, countryID: obj["PersonCountryID"] as! Int32, country: obj["Country"] as! String)
-                                    
-                                    let personObj = Person(personID: obj["PersonPersonID"] as! Int32, firstName: obj["FirstName"] as! String, lastName: obj["LastName"] as! String, gender: obj["Gender"] as! String, personLocation: locationObj)
-                                    
-                                    // Now you need to locate the correct row in WardenList and append the PersonObj
-                                    // to it's person array
-                                    //personList.index(where: <#T##(PersonListObj) throws -> Bool#>) Something like this...
-                                    if let i = self.personList.index(where: { $0.wardenObj.wardenID == obj["WardenID"] as! Int32 }) {
-                                        //self.personList.insert(personObj, at: i)
-                                        let currentPersonListObj = self.personList[i]
-                                        currentPersonListObj.personObj.append(personObj)
-                                        
-                                        self.personList.remove(at: i)
-                                        self.personList.insert(currentPersonListObj, at: i)
-                                    }
-                                }
-                            }
-                            DispatchQueue.main.async(execute: {
-                                //self.activityIndicator.stopAnimating()
-                                //self.errorText.text = "Invalid Credentials Supplied"
-                                print("Successful Person List Retrieval")
-                                
-                                //Pass the locations array to the next view
-                                //self.performSegue(withIdentifier: "locationsSegue", sender: self)
-                                
-                                //I need to work out how to trigger the segue to the new view from navigation view controller to table view controller.
-                                // The code errors here.
-                            })
-                        }
-                        else
-                        {
-                            DispatchQueue.main.async(execute: {
-                                //self.activityIndicator.stopAnimating()
-                                //self.errorText.text = "Invalid Credentials Supplied"
-                                print("Un-Successful Location Retrieval")
-                            })
-                        }
-                        
-                    }
-                    catch
-                    {
-                        print(error)
-                    }
-                }
-            }
-        }
-        
-        task.resume()
-    }
     
-    private func showWaitingView(tableView: UITableView) -> UIView {
-        let effectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
-        effectView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.addSubview(effectView)
-        NSLayoutConstraint.activate([
-            effectView.leadingAnchor.constraint(equalTo: tableView.leadingAnchor),
-            effectView.trailingAnchor.constraint(equalTo: tableView.trailingAnchor),
-            effectView.topAnchor.constraint(equalTo: tableView.topAnchor),
-            effectView.bottomAnchor.constraint(equalTo: tableView.bottomAnchor)
-            ])
-        
-        let spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
-        effectView.addSubview(spinner)
-        spinner.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            spinner.centerXAnchor.constraint(equalTo: tableView.centerXAnchor),
-            spinner.centerYAnchor.constraint(equalTo: tableView.centerYAnchor)
-            ])
-        
-        spinner.startAnimating()
-        return effectView
-    }
 
     // MARK: - Navigation
 
@@ -156,7 +33,6 @@ class PersonListNavigationController: UINavigationController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        
     }
 
 }
--- FireWarden/PersonListTableViewController.swift
+++ FireWarden/PersonListTableViewController.swift
@@ -21,11 +21,10 @@ class PersonListTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    
+        loadData()
         
-
-        // I need to split the JSON into to return statements
-        // Create a new PHP script to execute 2 separate querues
-        // and build enbedded json.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,20 +36,22 @@ class PersonListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return wardenList.count
+        if wardenList.count == 0 {
+            return 1
+        } else {
+            return wardenList.count
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        //return 1
-        
-        //Write a stored proc to return wardens with a count of people
-        //Put it in an array and use the section number to correspond to the 
-        //warden row and return it's count
-        
-        var sectionWarden = WardenCount()
-        sectionWarden = wardenList[section]
-        return Int(sectionWarden.personCount)
+        if wardenList.count == 0 {
+            return 1
+        } else {
+            var sectionWarden = WardenCount()
+            sectionWarden = wardenList[section]
+            return Int(sectionWarden.personCount)
+        }
     }
 
     
@@ -61,14 +62,154 @@ class PersonListTableViewController: UITableViewController {
         // Filter the PersonList by IndexPath.Section
         // and set the rows.
         
-        
-        
-        
+        if wardenList.count > 0 {
+            let currentPersonList = personList[indexPath.section]
+            let currentPerson = currentPersonList.personObj[indexPath.row]
+            
+            cell.textLabel?.text = "\(currentPerson.firstName) \(currentPerson.lastName)"
+            cell.detailTextLabel?.text = "\(currentPerson.personLocation.locationName), \(currentPerson.personLocation.floor), \(currentPerson.personLocation.country)"
+        }
 
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if wardenList.count > 0 {
+            let currentWarden = personList[section].wardenObj
+            return "Warden: \(currentWarden.firstName) \(currentWarden.lastName)"
+        } else {
+            return "Loading Data"
+        }
+    }
  
-
+    func loadData() {
+        //let waitingView = showWaitingView(tableView: tableView)
+        
+        let url = URL(string: "http://www.gratuityp.com/pk/GetData.php")
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        let postString = "ScriptName=sp_PersonList_GetList&ParamString=''"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if error != nil
+            {
+                print(error!)
+            }
+            else
+            {
+                defer {
+                    DispatchQueue.main.async {
+                        //waitingView.removeFromSuperview()
+                    }
+                }
+                if let content=data
+                {
+                    do
+                    {
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSArray
+                        print(myJson)
+                        if myJson.count > 0
+                        {
+                            self.wardenList.removeAll()
+                            for item in myJson {
+                                let obj = item as! NSDictionary
+                                
+                                if obj.count <= 5 {
+                                    //It's a WardenCount obj
+                                    let wardenCountObj = WardenCount(wardenID: obj["WardenID"] as! Int32, firstName: obj["FirstName"] as! String, lastName: obj["LastName"] as! String, personCount: obj["PersonCount"] as! Int32, sectionNumber: Int32(obj["SectionNumber"] as! String)!)
+                                    print(wardenCountObj)
+                                    self.wardenList.append(wardenCountObj)
+                                    
+                                    let wardenPersonList = [Person]()
+                                    let listObj = PersonListObj()
+                                    listObj.wardenObj = wardenCountObj
+                                    listObj.personObj = wardenPersonList
+                                    
+                                    self.personList.append(listObj)
+                                    
+                                    //This creates an array of wardens with a blank personObj array
+                                    //the else part should then find the correct warden and append the personObj to that array
+                                }
+                                else
+                                {
+                                    //it's a PersonListObj
+                                    
+                                    let locationObj = Location(locationID: obj["PersonLocationID"] as! Int32, locationName: obj["LocationName"] as! String, floor: obj["Floor"] as! String, countryID: obj["PersonCountryID"] as! Int32, country: obj["Country"] as! String)
+                                    
+                                    let personObj = Person(personID: obj["PersonPersonID"] as! Int32, firstName: obj["FirstName"] as! String, lastName: obj["LastName"] as! String, gender: obj["Gender"] as! String, personLocation: locationObj)
+                                    
+                                    // Now you need to locate the correct row in WardenList and append the PersonObj
+                                    // to it's person array
+                                    //personList.index(where: <#T##(PersonListObj) throws -> Bool#>) Something like this...
+                                    if let i = self.personList.index(where: { $0.wardenObj.wardenID == obj["WardenID"] as! Int32 }) {
+                                        //self.personList.insert(personObj, at: i)
+                                        let currentPersonListObj = self.personList[i]
+                                        currentPersonListObj.personObj.append(personObj)
+                                        
+                                        self.personList.remove(at: i)
+                                        self.personList.insert(currentPersonListObj, at: i)
+                                    }
+                                }
+                            }
+                            DispatchQueue.main.async(execute: {
+                                //self.activityIndicator.stopAnimating()
+                                //self.errorText.text = "Invalid Credentials Supplied"
+                                print("Successful Person List Retrieval")
+                                
+                                //Pass the locations array to the next view
+                                //self.performSegue(withIdentifier: "locationsSegue", sender: self)
+                                
+                                //I need to work out how to trigger the segue to the new view from navigation view controller to table view controller.
+                                // The code errors here.
+                                
+                                self.tableView.reloadData()
+                            })
+                        }
+                        else
+                        {
+                            DispatchQueue.main.async(execute: {
+                                //self.activityIndicator.stopAnimating()
+                                //self.errorText.text = "Invalid Credentials Supplied"
+                                print("Un-Successful Location Retrieval")
+                            })
+                        }
+                        
+                    }
+                    catch
+                    {
+                        print(error)
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func showWaitingView(tableView: UITableView) -> UIView {
+        let effectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
+        effectView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.addSubview(effectView)
+        NSLayoutConstraint.activate([
+            effectView.leadingAnchor.constraint(equalTo: tableView.leadingAnchor),
+            effectView.trailingAnchor.constraint(equalTo: tableView.trailingAnchor),
+            effectView.topAnchor.constraint(equalTo: tableView.topAnchor),
+            effectView.bottomAnchor.constraint(equalTo: tableView.bottomAnchor)
+            ])
+        
+        let spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        effectView.addSubview(spinner)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: tableView.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: tableView.centerYAnchor)
+            ])
+        
+        spinner.startAnimating()
+        return effectView
+    }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
